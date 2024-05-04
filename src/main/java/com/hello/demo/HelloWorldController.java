9bc676c15dc2e95a743532d833ac21fbe5a74007
jack <wangjack096@gmail.com>
2024-05-03 23:07:52 -0500

    first Hello World

---
+++ src/main/java/com/hello/demo/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.hello.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloWorld(){
+        return "Hello World";
+    }
+    }
