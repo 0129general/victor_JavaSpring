01e2287dcbab44fbf2adf19afe15fcf683638624
jack <wangjack096@gmail.com>
2024-05-05 17:32:39 -0500

    commit the line unused

--- src/main/java/com/hello/demo/model/GroupRepository.java
+++ src/main/java/com/hello/demo/model/GroupRepository.java
@@ -3,7 +3,7 @@ package com.hello.demo.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+// import java.util.List;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
     Group findByName(String name);
